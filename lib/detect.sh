b14d0d12d206099b0528c748f13f7a5e7e30eb0d
Nan Zhong <me@nanzho.ng>
2020-04-21 18:19:06 -0400

    Add support for writing .appsail/detect in bin/detect

--- lib/detect.sh
+++ lib/detect.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
 
+# shellcheck source=/dev/null
+source "$bp_dir/lib/utils/json.sh"
+
 detect_yarn_lock() {
   local build_dir=$1
   [[ -f "$build_dir/yarn.lock" ]]
+}
+
+write_detect_toml() {
+  local package_json=$1
+  local detect_toml=$2
+
+  mkdir -p $(dirname "$detect_toml")
+
+  build_script=$(json_get_key "$build_dir/package.json" ".scripts.build")
+  run_script=$(json_get_key "$build_dir/package.json" ".scripts.start")
+
+  if [[ $build_script ]] ; then
+    cat <<TOML >> "$detect_toml"
+[[build]]
+command = "yarn build"
+TOML
+  fi
+
+  if [ "null" != "$(jq -r .scripts.start < "$package_json")" ]; then
+    cat <<TOML >> "$detect_toml"
+[[run]]
+type = "web"
+command = "yarn start"
+TOML
+  fi
 }
